bf7b77aeaa03801baff7c8e22bec997e3fd66dbe
mattmc3 <mattmc3@gmail.com>
2021-12-02 13:16:22 -0500

    Refactor

--- functions/day2.fish
+++ functions/day2.fish
@@ -2,69 +2,58 @@ set --global horizontal_position
 set --global depth_position
 set --global aim
 
-function day2part1 \
-    --description "https://adventofcode.com/2021/day/2" \
-    --argument-names datafile
+function day2 \
+    --description "https://adventofcode.com/2021/day/2: `usage - day2 1 data.txt`" \
+    --argument-names part datafile
 
-    set --global horizontal_position 0
-    set --global depth_position 0
-    set --local movement_data (cat $datafile)
-    for instruction in $movement_data
-        move (string split ' ' $instruction)
+    if test "$part" -ne 1 && test "$part" -ne 2
+        echo "Expecting part number 1 or 2 '$part'" >&2 && return 1
     end
-
-    echo "horizontal_position: $horizontal_position"
-    echo "depth_position: $depth_position"
-    echo "multiplied: " (math $horizontal_position '*' $depth_position)
-end
-
-function move \
-    --description "https://adventofcode.com/2021/day/2" \
-    --argument-names direction distance
-
-    switch $direction
-        case forward
-            set horizontal_position (math $horizontal_position + $distance)
-        case down
-            set depth_position (math $depth_position + $distance)
-        case up
-            set depth_position (math $depth_position - $distance)
-        case '*'
-            echo "Unexpected direction: $direction" >&2 && return 1
-    end
-end
-
-function day2part2 \
-    --description "https://adventofcode.com/2021/day/2" \
-    --argument-names datafile
 
     set --global horizontal_position 0
     set --global depth_position 0
     set --global aim 0
     set --local movement_data (cat $datafile)
     for instruction in $movement_data
-        move2 (string split ' ' $instruction)
+        move $part (string split ' ' $instruction)
     end
 
     echo "horizontal_position: $horizontal_position"
     echo "depth_position: $depth_position"
     echo "aim: $aim"
     echo "multiplied: " (math $horizontal_position '*' $depth_position)
+
+    set --erase horizontal_position
+    set --erase depth_position
+    set --erase aim
 end
 
-function move2 \
+function move \
     --description "https://adventofcode.com/2021/day/2" \
-    --argument-names direction distance
+    --argument-names algorithm direction distance
 
-    switch $direction
-        case forward
-            set horizontal_position (math $horizontal_position + $distance)
-            set depth_position (math $depth_position + (math $distance '*' $aim))
-        case down
-            set aim (math $aim + $distance)
-        case up
-            set aim (math $aim - $distance)
-        case '*'
-            echo "Unexpected direction: $direction" >&2 && return 1
+    if test $algorithm -eq 1
+        switch $direction
+            case forward
+                set horizontal_position (math $horizontal_position + $distance)
+            case down
+                set depth_position (math $depth_position + $distance)
+            case up
+                set depth_position (math $depth_position - $distance)
+            case '*'
+                echo "Unexpected direction: $direction" >&2 && return 1
+        end
+    else
+        switch $direction
+            case forward
+                set horizontal_position (math $horizontal_position + $distance)
+                set depth_position (math $depth_position + (math $distance '*' $aim))
+            case down
+                set aim (math $aim + $distance)
+            case up
+                set aim (math $aim - $distance)
+            case '*'
+                echo "Unexpected direction: $direction" >&2 && return 1
+        end
     end
 end
