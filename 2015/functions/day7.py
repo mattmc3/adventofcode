bc257a7c51d0c33e25ddc6a131f027553298fdb7
mattmc3 <mattmc3@gmail.com>
2021-12-06 15:37:30 -0500

    Add 2015 day 7

--- 2015/functions/day7.py
+++ 2015/functions/day7.py
@@ -3,6 +3,8 @@
 from pathlib import Path
 import re
 
+memoizer = {}
+
 def main():
     datafile = Path(__file__).parent.parent / "data" / "day7.dat"
 
@@ -14,50 +16,59 @@ def main():
         in_part, out_part = line.split(" -> ")
         logic[out_part] = in_part
 
+    # for part2, uncomment this line
+    logic["b"] = 3176
+
     # for k in sorted(logic.keys()):
     #     val = get_value(logic, k)
     #     # convert negatives to unsigned 16-bit
     #     if val < 0:
     #         val = val+2**16
     #     print(f"{k}: {val}")
+
     val = get_value(logic, "a")
     print(f"a: {val}")
+    # 3176 part1
+    # 14710 part2
 
 def get_value(logic, wire):
-    print(f"get_value called for {wire}")
+    if wire in memoizer:
+        return memoizer[wire]
+
     if type(wire) is int or wire.isnumeric():
-        return int(wire)
+        memoizer[wire] = int(wire)
+        return memoizer[wire]
 
     if wire not in logic:
         raise Exception(f"Unexpected wire: {wire}")
 
     instruction = logic[wire]
     if type(instruction) is int or instruction.isnumeric():
-        return int(instruction)
-    print(instruction)
-    parts = instruction.split(' ')
+        memoizer[wire] = int(instruction)
+        return memoizer[wire]
 
+    parts = instruction.split(' ')
     if parts[0] == "NOT":
         val = get_value(logic, parts[1])
-        return ~val
+        memoizer[wire] = ~val
     elif len(parts) == 1 and parts[0] in logic:
-        return get_value(logic, parts[0])
+        memoizer[wire] = get_value(logic, parts[0])
     elif len(parts) != 3:
         raise Exception(f"Unexpected instruction: {instruction}")
     else:
         val1 = get_value(logic, parts[0])
         val2 = get_value(logic, parts[2])
         if parts[1] == "AND":
-            return val1 & val2
+            memoizer[wire] = val1 & val2
         elif parts[1] == "OR":
-            return val1 | val2
+            memoizer[wire] = val1 | val2
         elif parts[1] == "LSHIFT":
-            return val1 << val2
+            memoizer[wire] = val1 << val2
         elif parts[1] == "RSHIFT":
-            return val1 >> val2
+            memoizer[wire] = val1 >> val2
         else:
             raise Exception(f"Unexpected logic operator: {parts[1]}")
-
+    return memoizer[wire]
 
 if __name__ == "__main__":
     main()
