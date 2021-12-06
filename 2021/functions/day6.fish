fb4855aa09cc0f5ef73a3acaf100a6a237cf54d5
mattmc3 <mattmc3@gmail.com>
2021-12-06 14:51:27 -0500

    Add day 6 fish solution

--- 2021/functions/day6.fish
+++ 2021/functions/day6.fish
@@ -1,60 +1,47 @@
 function day6 \
-    --description "https://adventofcode.com/2021/day/6 - usage: day6 part1 datafile.dat" \
-    --argument-names part datafile
-
-    set part (string match --regex '.$' $argv[1])
-    if test "$part" -ne 1 && test "$part" -ne 2
-        echo "Expecting part number 1 or 2 '$part'" >&2 && return 1
-    end
-    test -n "$datafile"; \
-        or set datafile (realpath (status dirname)/../data/day6.dat)
-
-    day6part$part $datafile
-    day6cleanup
-end
-
-function day6part1 \
+    --description "https://adventofcode.com/2021/day/6 - usage: day6 datafile.dat" \
     --argument-names datafile
 
-    set --local lantern_fish (string split ',' <$datafile)
-    echo $lantern_fish
-    for day in (seq 80)
-        set lantern_fish (breed $lantern_fish)
-        # echo $lantern_fish
-        echo "day: $day"
-    end
-    echo "solution:" (count $lantern_fish)
-end
+    test -f "$datafile"; or echo >&2 "file expected" && return 1
+    set --local lantern_fish_timers (string split ',' <$datafile)
+    set --local total_days 256
+    set --local new_fish_each_day (string split '' (string repeat -n $total_days 0))
+    set --local fish_alive_each_day (string split '' (string repeat -n $total_days 0))
 
-function breed
-    set --local new_fish
-    for fish_timer in $argv
-        if test $fish_timer -eq 0
-            set --append new_fish 8
-            echo 6
+    # initialize
+    # we use the fish we know about, and set their breeding schedule
+    for timer in $lantern_fish_timers
+        for day in (seq $total_days)
+            # (day+(7-timer)-1)%7
+            if test (math \($day + \(7 - $timer\) - 1\) % 7) -eq 0
+                set new_fish_each_day[$day] (math $new_fish_each_day[$day] + 1)
+            end
+        end
+    end
+
+    # then we calculate for each remaining day
+    for day in (seq $total_days)
+        set new_fish $new_fish_each_day[$day]
+        set yesterday (math $day - 1)
+        if test $yesterday -lt 1
+            set fish_alive_each_day[$day] (math (count $lantern_fish_timers) + $new_fish)
         else
-            math $fish_timer - 1
+            set fish_alive_each_day[$day] (math $fish_alive_each_day[$yesterday] + $new_fish)
+        end
+
+        # new fish breed and produce a new fish after 9 days,
+        # and then every 7th day after that
+        set --local nine_days_later (math $day + 9)
+        if test $nine_days_later -le $total_days
+            set $new_fish_each_day[$nine_days_later] = (math $new_fish_each_day[$nine_days_later] + $new_fish)
+
+            for breeding_day in (seq $nine_days_later $total_days)
+                if test (math \($breeding_day - $day - 2\) % 7) -eq 0
+                    set new_fish_each_day[$breeding_day] (math $new_fish_each_day[$breeding_day] + $new_fish)
+                end
+            end
         end
     end
-    string split ' ' $new_fish
-end
 
-function day6part2
-    set --local lantern_fish 6 6 6
-    echo $lantern_fish
-    for day in (seq 50)
-        set lantern_fish (breed $lantern_fish)
-        # echo $lantern_fish
-        echo "day: $day, count:" (count $lantern_fish)
-    end
-    echo "solution:" (count $lantern_fish)
-end
-
-function day6cleanup
-    for var in (set --names)
-        if string match --quiet --regex '^aoc_' $var
-            echo "cleaning aoc var: $var $$var"
-            set --erase $var
-        end
-    end
+    echo "The solution is: $fish_alive_each_day[-1]"
 end
